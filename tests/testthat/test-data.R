567bc3020023c1714ee25c557bdf852668d78fef
Thijs van Lankveld <53601273+vanlankveldthijs@users.noreply.github.com>
2024-12-11 14:57:42 +0100

    Apply suggestions from code review
    Replaced test_city variable by bucharest
    
    Co-authored-by: Claudiu Forgaci <33600128+cforgaci@users.noreply.github.com>

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,9 +1,9 @@
 test_that("the test data can be loaded", {
-  streets <- test_city$streets
+  streets <- bucharest$streets
   expect_false(is.null(streets))
   expect_true(length(streets) > 0)
 
-  river <- test_city$river_centerline
+  river <- bucharest$river_centerline
   expect_false(is.null(river))
   expect_true(length(river) > 0)
 })
