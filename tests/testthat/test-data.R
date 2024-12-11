a0c604ad77baf4a1284d2c973cea650d76af03f9
thijsvl <t.vanlankveld@esciencecenter.nl>
2024-12-11 14:59:06 +0100

    Removed redundant expect lines from data test.

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,9 +1,7 @@
 test_that("the test data can be loaded", {
   streets <- bucharest$streets
-  expect_false(is.null(streets))
   expect_true(length(streets) > 0)
 
   river <- bucharest$river_centerline
-  expect_false(is.null(river))
   expect_true(length(river) > 0)
 })
