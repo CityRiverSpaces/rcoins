25e2452b406645a98db0f37daef172da2711065e
Francesco Nattino <f.nattino@esciencecenter.nl>
2025-05-27 11:34:06 +0200

    fix call to on.exit

--- R/example_data.R
+++ R/example_data.R
@@ -19,7 +19,7 @@ get_example_data <- function() {
   temp_file <- tempfile(fileext = ".gpkg")
   # temporarily increate timeout, reset value on exit
   op <- options(timeout = 300)
-  on.exit(op)
+  on.exit(options(op))
   download.file(url_osm, destfile = temp_file, mode = "wb", quiet = TRUE)
   layers <- c("streets", "river_centerline")
   sapply(layers, \(x) sf::st_read(temp_file, layer = x, quiet = TRUE))
